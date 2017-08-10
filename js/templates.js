ddb94b75a6757068350ad013ed2231cc23638272
pain <niveditadhenge@gmail.com>
2017-08-10 20:22:17 +0530

    header

--- js/templates.js
+++ js/templates.js
@@ -1,1 +1,19 @@
-alert();
+//image slide
+
+var slide = document.getElementsByClassName("slide_img");
+var index=0;
+slideimg(0);
+function slideimg(x){
+    setInterval(function(x){
+
+        for (var i=0;i<slide.length;i++){
+            slide[i].style.display = "none";
+        }
+        index++
+            if(index == slide.length){
+                index=0;
+            };
+        slide[index].style.display = "block";
+
+    },3000);
+};
